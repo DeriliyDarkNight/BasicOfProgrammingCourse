285ecf754f872190e3e9a9e3ca85385e10336c4d
DeriliyDarkNight <dakovlev729@gmail.com>
2022-02-23 07:35:55 +0300

    task 2

--- libs/data_structures/matrix/tascs_lab5B.c
+++ libs/data_structures/matrix/tascs_lab5B.c
@@ -2,3 +2,14 @@
 // Created by DeriliyDarkNight on 23.02.2022.
 //
 
+#include "tascs_lab5B.h"
+#include "../../algorithms/array/array.h"
+#include <malloc.h>
+#include <iso646.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void sortRowsByMaxElement(matrix m) {
+    selectionSortRowsMatrixByRowCriteria(m, getMax);
+}
--- libs/data_structures/matrix/tascs_lab5B.h
+++ libs/data_structures/matrix/tascs_lab5B.h
@@ -5,6 +5,10 @@
 #ifndef MAIN_C_TASCS_LAB5B_H
 #define MAIN_C_TASCS_LAB5B_H
 
+#include "matrix.h"
+
+// сортирует строки матрицы m по неубыванию наибольших элементов строк
+void sortRowsByMaxElement(matrix m);
 
 
 #endif //MAIN_C_TASCS_LAB5B_H
